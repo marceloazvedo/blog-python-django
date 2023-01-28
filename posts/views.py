88a848e8f8d26e0c39ccd12d34416c89f30694a7
Marcelo Azevedo <marceloazvedo1@gmail.com>
2023-01-28 20:32:22 -0300

    adiciona template com bootstrap

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,33 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+
+from .models import Post
+
+POST_LIST_TEMPLATE = '''
+        <h1>{title}</h1><br>
+        <h3>tags: {tags}</h3><br>
+        <div><i>{content}</i><div>
+        <br>\n
+    '''
+
+def format_post(post):
+    return POST_LIST_TEMPLATE.format(
+        title=post.title,
+        tags=post.tags,
+        content=post.content,
+    )
 
 def index(request):
-    return HttpResponse("Essa é a página da listagem de POSTs")
+    context = {
+        'all_posts': Post.objects.order_by('-pub_date')
+    }
+
+    return render(request, 'posts/index.html', context)
+
+def show(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+        output = format_post(post)
+    except Post.DoesNotExist:
+        raise Http404("Esse post não existe.")
+    return HttpResponse(output)
